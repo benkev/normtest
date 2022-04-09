3945efdc3a4c7fcb85c5db8cca236e6085cc1501
Leonid Benkevitch <lbenkev@gmail.com>
2022-04-09 05:05:52 -0400

    ...

--- normality_test_1.py
+++ normality_test_1.py
@@ -9,24 +9,31 @@ import scipy.stats
 from numpy.random import seed
 from numpy.random import normal
 
-seed(1)
-dat = normal(loc=0.0, scale=1.0, size=int(1e8))
+N = int(1e5)
+alpha = 0.95
+
+# seed(12)
+dat = normal(loc=0.0, scale=1.0, size=N)
 #dat = normal(loc=2.4, scale=3.7, size=int(1e3))
 
-pl.figure(); pl.hist(dat, bins=20) 
-pl.show()
-pl.grid(1)
+# pl.figure(); pl.hist(dat, bins=20) 
+# pl.show()
+# pl.grid(1)
 
-h =1.0
+h = 0.25                  # Interval length
+#h = 1.0                   # Interval length
+#h = 0.125                 # Interval length
 lx = np.floor(dat.min()) # Left interval limit
 rx = np.ceil(dat.max())  # Right interval limit
-bins = np.arange(lx, rx+1)     # Bin edges
-x = 0.5 + np.arange(lx, rx)  # Bin middle points
-frq, binedg = np.histogram(dat, bins=bins)
+nedges = int((rx - lx)/h) + 1
+edges = np.linspace(lx, rx, nedges)    # Bin edges
+x = edges[:-1] + 0.5*h
+# x = h*(0.5 + np.arange(lx, rx))  # Bin middle points
+frq, binedg = np.histogram(dat, bins=edges)
+nfrq = len(frq)
 
-N = np.sum(frq)
+# raise SystemExit
 
-x2n = (x**2)*frq
 xmean = np.sum(x*frq)/N
 xsig = np.sqrt(np.sum((x**2)*frq)/N - xmean**2)
 z = (x - xmean)/xsig  # Standardized 
@@ -35,12 +42,12 @@ tfrq = (h*N/xsig)*f  # Theoretical frequency
 chi2 = np.sum((frq - tfrq)**2/tfrq)
 
 #
-# Critical value of chi2(k) with 10 degrees of freedom, for significance 0.95
+# Critical value of chi2(k) with k degrees of freedom, for significance 0.95
 #
-# dat is normal (at significance 0.95) if chi2 < chi2cr
+# dat is normal (at significance alpha) if chi2 < chi2cr
 #
-k = len(x) - 2 - 1
-chi2cr = scipy.stats.chi2.ppf(1-0.95, k)
+k = nfrq - 2 - 1
+chi2cr = scipy.stats.chi2.ppf(alpha, k)
 
 pl.figure();
 pl.plot(x, frq, 'o'); pl.grid(1)
@@ -51,6 +58,8 @@ pl.show()
 
 cmps = ' < ' if chi2 < chi2cr else ' > '
 norm_or_not = ' -- Normal ' if chi2 < chi2cr else ' -- NOT Normal '
-print('chi2 = %6.2f' % chi2, ', chi2cr = %6.2f' % chi2cr)
+print('N = ', N, ', nfrq = ', nfrq)
+print('chi2_observed = %6.2f' % chi2, ', chi2_critical = %6.2f' % chi2cr, \
+      ' at k = %d degrees of freedom' % k)
 print('chi2_observed' + cmps + 'chi2_critical: ' + \
-      norm_or_not + ' at 0.95 significance level')
+      norm_or_not + ' at %4.2f significance level' % alpha)
