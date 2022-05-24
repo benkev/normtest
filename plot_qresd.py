a8946b9ba8636b9dfe1a025d174dcb7fedbc5366
Leonid Benkevitch <lbenkev@gmail.com>
2022-05-23 22:14:47 -0400

    Testing m5b file streams for normality/gaussianity, test_m5b_thopt.c, works correctly. Proven by comparing its results with thouse of test_m5b_thopt.py. Next: GPU implementation.

--- plot_qresd.py
+++ plot_qresd.py
@@ -4,10 +4,44 @@ import matplotlib.pyplot as pl
 q = np.loadtxt('qresd.txt')
 r = np.reshape(q[:16*16,:], (64,64))
 
-pl.figure(); pl.pcolormesh(r); pl.colorbar(shrink=0.8)
+pl.figure(figsize=(9,7))
+# pl.pcolormesh(r, cmap=pl.cm.hot)
+pl.pcolormesh(r, cmap=pl.cm.jet)
+# pl.pcolormesh(r)
+pl.colorbar(shrink=0.8)
 pl.title('$\chi^2$ of Difference bw Normal and M5B Quantiles')
 pl.xlabel('4 Columns 16 Channels Each')
-pl.ylabel('$\chi^2$ for 64 Words')
+pl.ylabel('$\chi^2$ for 64 Frames')
+
+pl.plot([16,16], [0,64], 'w', lw=0.5)
+pl.plot([32,32], [0,64], 'w', lw=0.5)
+pl.plot([48,48], [0,64], 'w', lw=0.5)
+
+# pl.plot([0,64], [48,48], 'w', lw=0.5)
+# pl.plot([0,64], [32,32], 'w', lw=0.5)
+# pl.plot([0,64], [16,16], 'w', lw=0.5)
+
+
+th = np.loadtxt('thresh.txt')
+rh = np.reshape(th[:16*16,:], (64,64))
+
+pl.figure(figsize=(9,7))
+# pl.pcolormesh(rh, cmap=pl.cm.hot)
+pl.pcolormesh(rh, cmap=pl.cm.jet)
+# pl.pcolormesh(rh)
+pl.colorbar(shrink=0.8)
+pl.title('Optimal Quantization Thresholds')
+pl.xlabel('4 Columns 16 Channels Each')
+pl.ylabel('Q-Thresholds for 64 Frames')
+
+pl.plot([16,16], [0,64], 'w', lw=0.5)
+pl.plot([32,32], [0,64], 'w', lw=0.5)
+pl.plot([48,48], [0,64], 'w', lw=0.5)
+
+# pl.plot([0,64], [48,48], 'w', lw=0.5)
+# pl.plot([0,64], [32,32], 'w', lw=0.5)
+# pl.plot([0,64], [16,16], 'w', lw=0.5)
+
 
 pl.show()
 
