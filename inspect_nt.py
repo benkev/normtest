2a32b2fa2c2b4a5cc0c7523165fcd7661f20d48e
Leonid Benkevitch <lbenkev@gmail.com>
2022-11-10 18:57:44 -0500

    ...

--- inspect_nt.py
+++ inspect_nt.py
@@ -19,6 +19,10 @@ opts, args = getopt.gnu_getopt(sys.argv[1:], '')
 
 fm5b = args[0]
 ftst = args[1]
+frm0 = int(args[2])
+frm1 = int(args[3])
+
+
 
 fm5b_base = os.path.basename(fm5b)
 fm5b_base = os.path.splitext(fm5b_base)[0]
@@ -26,8 +30,13 @@ fm5b_base = os.path.splitext(fm5b_base)[0]
 fquantls = glob.glob("nt_quantl_*" + fm5b_base + "*" + ftst +".bin")
 fthreshs = glob.glob("nt_thresh_*" + fm5b_base + "*" + ftst +".bin")
 
+if len(fquantls) == 0 or len(fthreshs) == 0:
+    print("File *%s*.bin with the timestamp \"%s\" not found." % \
+          (fm5b_base, ftst))
+    raise SystemExit
+
 if len(fquantls) > 1 or len(fthreshs) > 1:
-    print("Ambiguous timestamp. Give more detail.")
+    print("Ambiguous timestamp \"%s\". Give more detail." % ftst)
     raise SystemExit
 
 #raise SystemExit
@@ -35,12 +44,22 @@ if len(fquantls) > 1 or len(fthreshs) > 1:
 fquantl_name = fquantls[0]
 fthresh_name = fthreshs[0]
 
-fquantl = open(fquantl_name, "rb")
-fthresh = open(fthresh_name, "rb")
+#
+# Find file offsetts to read the data from
+#
+n_frms = frm1 - frm0
 
+cnt_q = 16*4*n_frms          # float32 words
+offs_qdat = 16*4*4*frm0 + 16 # Bytes
 
-fquantl.close()
-fthresh.close()
+cnt_t = 16*n_frms            # float32 words
+offs_tdat = 16*4*frm0 + 16   # Bytes
+
+fquantl = np.fromfile(fquantl_name, dtype=np.float32, \
+                      offset=offs_qdat, count=cnt_q)
+fthresh = np.fromfile(fthresh_name, dtype=np.float32, \
+                      offset=offs_tdat, count=cnt_t)
+
 
 
 
