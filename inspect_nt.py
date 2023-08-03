6ce16a0b43ca28f09ffd05f0fd5f3dc3fb15b212
Leonid Benkevitch <lbenkev@gmail.com>
2023-08-02 22:09:45 -0400

    inspect_nt.py done. Now, starting work on the memo.

--- inspect_nt.py
+++ inspect_nt.py
@@ -125,6 +125,8 @@ print('Chi^2 critical value at significance %.2f and df = %d: %.2f' % \
 #
 # Plot
 #
+pl.ion()  # Interactive mode; pl.ioff() - revert to non-interactive.
+print("pl.isinteractive() -> ", pl.isinteractive())
 
 # Fthr = F(-thr)
 # q_nor = np.array([Fthr, 0.5-Fthr, 0.5-Fthr, Fthr])    # Normal quantilles
@@ -143,24 +145,29 @@ print('Chi^2 critical value at significance %.2f and df = %d: %.2f' % \
 fnorm = lambda x: (1/np.sqrt(2*np.pi))*(np.exp(-0.5*x**2))  # Normal PDF (0,1)
 Fnorm = lambda x: 0.5*(1 + erf(x/np.sqrt(2)))               # Normal CDF (0,1)
 
-hsep = 0.3
+#hsep = 0.3
+hsep = 0.2
 
 # thr = 0.1*np.arange(1,17)
-threshold = np.array([0.2, 0.3, 0.5, 0.6745, 0.8, 1.0, 1.2, 1.5, 2.0, \
-                      0.5, 0.6745, 0.8, 1.0, 1.2, 1.5, 2.0]);
 xrul = np.linspace(-3., 3., 1001)
 xloc = np.array([-2, -0.5, 0.5, 1.5])
 f_pdf = (1/np.sqrt(2*np.pi))*(np.exp(-0.5*xrul**2))  # Normal PDF (0,1)
-f_pdf = (hsep-0.05)*f_pdf/f_pdf.max()
+f_pdf = (hsep-0.02)*f_pdf/f_pdf.max() # Make curve just under separation line
 
 pl.figure(figsize=(8,8))
-pl.figtext(0.5, 0.96, r"Normal PDF Quantiles Separated by $-\infty, " \
-           r"-\theta, \, 0, +\theta, +\infty$", fontsize=16, ha='center')
+pl.figtext(0.5, 0.96, r"Observed \& Normal PDF Quantiles Separated by " \
+           r"$-\infty, -\theta, \, 0, +\theta, +\infty$",
+           fontsize=16, ha='center')
+
 for ich in range(16):
     pl.subplot(4, 4, ich+1)
 
     thr = th_obs[ich]
-#    xloc =   np.array([-2, -thr[ich], thr[ich], 2])
+    qua = 1.9*q_obs[ich,:]/2500
+
+    Fthr = Fnorm(-thr)
+    qnor = 1.9*np.array([Fthr, 0.5-Fthr, 0.5-Fthr, Fthr])
+    
     barloc = np.array([-2.5, -thr/2, thr/2, 2.5])
     pl.plot(xrul, f_pdf, 'orange')
     pl.fill_between(xrul, f_pdf, where=(-10<xrul)&(xrul<=-thr), color='b')
@@ -170,27 +177,34 @@ for ich in range(16):
 
     xl = pl.xlim()
     pl.plot([xl[0],xl[1]], [hsep,hsep], 'k--', lw=1) # Horiz. separation line
-    pl.plot([-thr,-thr], [0,0.6], 'k--', lw=0.7)   # 
-    pl.plot([thr,thr], [0,0.6], 'k--', lw=0.7)     #
-    pl.xticks([-2,0,2], ["$-2\sigma$", "$0$", "$2\sigma$"], fontsize=12)
+    # pl.plot([-thr,-thr], [0,0.6], 'k--', lw=0.7)   # Vert. line -threshold
+    # pl.plot([thr,thr], [0,0.6], 'k--', lw=0.7)     # Vert. line +threshold
+    pl.plot([-thr,-thr], [0,0.75], 'k--', lw=0.7)   # Vert. line -threshold
+    pl.plot([thr,thr], [0,0.75], 'k--', lw=0.7)     # Vert. line +threshold
+    pl.xticks([-2,0,2], ["$-2\sigma$", "$0$", "$2\sigma$"], fontsize=9)
     pl.yticks([])
-    pl.text(-thr-0.7, 0.65, r"$-\theta$")
-    pl.text(thr+0.05, 0.65, r"$+\theta$")
+    # pl.text(-thr-0.7, 0.65, r"$-\theta$")
+    # pl.text(thr+0.05, 0.65, r"$+\theta$")
+    pl.text(-thr-0.7, 0.78, r"$-\theta$", fontsize=8)
+    pl.text(thr+0.05, 0.78, r"$+\theta$", fontsize=8)
     
-    Fthr = Fnorm(-thr)
-    qnor = np.array([Fthr, 0.5-Fthr, 0.5-Fthr, Fthr])
-    bw = 0.15 if thr < 0.4 else 0.25
-    pl.bar(barloc, qnor, bw, hsep+0.05, color='red', edgecolor='k')
-    pl.ylim(-0.05, 0.85)
-    pl.text(xl[0], 0.77, r"$\theta=\pm%4.2f$" % thr, fontsize=10)
-    pl.text(xl[1]-1.5, 0.76, "ch %2d" % ich, fontsize=10)
-    pl.text(xl[1]-1.5, 0.60, r"$\chi^2=%4.1f$" % c2_obs[ich], fontsize=10)
+    bw = 0.15 if thr < 0.4 else 0.25  # Width of the histogram boxes
+    pl.bar(barloc, qnor, bw*2, hsep+0.03, color='c')   # , edgecolor='k')
+    pl.bar(barloc, qua, bw/2, hsep+0.03, color='k')
+    pl.plot(barloc, qua+hsep+0.05, 'ro', markersize=1.5)
+    pl.ylim(-0.03, 1.0)
+    # pl.ylim(-0.05, 0.85)
+    pl.text(xl[0], 0.9, r"$\theta=\pm%4.2f$;" % thr, fontsize=8)
+    pl.text(-0.6, 0.9, r"$\chi^2=%4.1f$;" % c2_obs[ich], fontsize=8)
+    pl.text(xl[1]-1.1, 0.9, "ch %2d" % ich, fontsize=8)
     #pl.grid(1)
 
 
 # pl.figtext(0.5, 0.05, r"* Quantization Thresholds $\pm\theta$: " \
 #            r"MUST be $|\theta|\, > 0.6745 \, \sigma$", \
 #            fontsize=20, ha='center')
+pl.figtext(0.5, 0.005, r"File: %s; Start frame %d, frames %d" %
+           (fm5b, frm0, n_frms), fontsize=13, ha='center')
 
 pl.subplots_adjust(top=0.935, bottom=0.055, left=0.035, right=0.965,
                    hspace=0.2, wspace=0.2)
