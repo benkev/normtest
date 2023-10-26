120942345631e4f4aeb4bd7c4ead29f32d54f1be
Leonid Benkevitch <lbenkev@gmail.com>
2023-10-26 14:54:30 -0400

    The report: Add some scripts to \subsection{Miscellaneous}.

--- hist_thresh.py
+++ hist_thresh.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as pl
 import getopt, glob
 
+pl.ion()
 pl.rcParams['text.usetex'] = True # Use LaTeX in Matplotlib text
 
 opts, args = getopt.gnu_getopt(sys.argv[1:], '')
@@ -22,46 +23,46 @@ fm5b_base = os.path.basename(fm5b)
 fm5b_base = os.path.splitext(fm5b_base)[0]
 
 if ftst:
-    fquantls = glob.glob("nt_hist_*" + fm5b_base + "*" + ftst +".bin")
+    fhists = glob.glob("nt_hist_*" + fm5b_base + "*" + ftst +".bin")
     fthreshs = glob.glob("nt_thresh_*" + fm5b_base + "*" + ftst +".bin")
-    fresidls = glob.glob("nt_residl_*" + fm5b_base + "*" + ftst +".bin")
+    fchi2s = glob.glob("nt_chi2_*" + fm5b_base + "*" + ftst +".bin")
 else:
-    fquantls = glob.glob("nt_hist_*" + fm5b_base + "*.bin")
+    fhists = glob.glob("nt_hist_*" + fm5b_base + "*.bin")
     fthreshs = glob.glob("nt_thresh_*" + fm5b_base + "*.bin")
-    fresidls = glob.glob("nt_residl_*" + fm5b_base + "*.bin")
+    fchi2s = glob.glob("nt_chi2_*" + fm5b_base + "*.bin")
     
-if len(fquantls) == 0 or len(fthreshs) == 0 or len(fresidls) == 0:
-#if len(fthreshs) == 0 or len(fresidls) == 0:
+if len(fhists) == 0 or len(fthreshs) == 0 or len(fchi2s) == 0:
+#if len(fthreshs) == 0 or len(fchi2s) == 0:
     print("Files *%s*.bin with the timestamp \"%s\" not found." % \
           (fm5b_base, ftst))
     raise SystemExit
 
-if ftst and (len(fquantls) > 1 or len(fthreshs) > 1 or len(fresidls) > 1):
-#if ftst and (len(fthreshs) > 1 or len(fresidls) > 1):
+if ftst and (len(fhists) > 1 or len(fthreshs) > 1 or len(fchi2s) > 1):
+#if ftst and (len(fthreshs) > 1 or len(fchi2s) > 1):
     print("Ambiguous timestamp \"%s\". Give more detail." % ftst)
     raise SystemExit
 
 #raise SystemExit
 
 if ftst:
-    fquantl_name = fquantls[0]
+    fhist_name = fhists[0]
     fthresh_name = fthreshs[0]
-    fresidl_name = fresidls[0]
+    fchi2_name = fchi2s[0]
 else:
-    fquantl_name = max(fquantls, key=os.path.getctime)
+    fhist_name = max(fhists, key=os.path.getctime)
     fthresh_name = max(fthreshs, key=os.path.getctime)
-    fresidl_name = max(fresidls, key=os.path.getctime)
+    fchi2_name = max(fchi2s, key=os.path.getctime)
     
     
 
 t0 = np.fromfile(fthresh_name, dtype=np.float32)
 thresh = t0.reshape((len(t0)//16, 16))
 
-# r0 = np.fromfile(fresidl_name, dtype=np.float32)
-# q0 = np.fromfile(fquantl_name, dtype=np.float32)
+# r0 = np.fromfile(fchi2_name, dtype=np.float32)
+# q0 = np.fromfile(fhist_name, dtype=np.float32)
 
-# quantl = q0.reshape((len(q0)//64, 16, 4)) / 2500.
-# residl = r0.reshape((len(r0)//16, 16))
+# hist = q0.reshape((len(q0)//64, 16, 4)) / 2500.
+# chi2 = r0.reshape((len(r0)//16, 16))
 
 nfrm = len(t0)//16
 
